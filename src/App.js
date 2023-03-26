d67c21e86a72b7bd346d5f14813fb3e73c704cf9
michelpinto <michabbe9@gmail.com>
2023-03-26 11:15:30 +0200

    portfolio refactor

--- src/App.js
+++ src/App.js
@@ -1,53 +1,19 @@
-import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
-import Projects from "./components/Projects/Projects";
-import About from "./components/About/About";
-import Footer from "./components/Footer/Footer";
-import styled from "styled-components";
-import Animation from "./components/BackgroundAnimation/Animation";
-import smoothscroll from "smoothscroll-polyfill";
-
-const Section = styled.section`
-    display: ${(props) => (props.grid ? "grid" : "flex")};
-    flex-direction: ${(props) => (props.row ? "row" : "column")};
-    padding: ${(props) => (props.nopadding ? "0" : "32px 6.5rem 0")};
-    margin: 0;
-    /* max-width: 1040px; */
-    box-sizing: content-box;
-    position: relative;
-    overflow: hidden;
-    grid-template-columns: 1fr 1fr;
-
-    @media ${(props) => props.theme.breakpoints.md} {
-        padding: 0 24px 0;
-        display: flex;
-        flex-direction: column;
-    }
-
-    @media screen and (max-width: 400px) {
-        padding: 1rem;
-    }
-
-    @media screen and (max-width: 350px) {
-        padding: 0;
-    }
-`;
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import Projects from './components/Projects/Projects';
+import styled from 'styled-components';
+import smoothscroll from 'smoothscroll-polyfill';
 
 smoothscroll.polyfill();
 
 function App() {
-    return (
-        <div>
-            <Header />
-            <Section grid>
-                <Hero />
-                <Animation />
-            </Section>
-            <Projects />
-            <About />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Projects />
+    </div>
+  );
 }
 
 export default App;
