0f230a8ddf01e43a38879774d96f0e1c35cc6153
michelpinto <michabbe9@gmail.com>
2024-02-29 13:52:14 +0200

    changing content

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,8 +5,8 @@ const Hero = () => {
     <Container>
       <Title>Hi, I'm Michel</Title>
       <p>
-        I’m a self-taught Frontend developer with over 1 year and a half of
-        professional experience in{' '}
+        I’m a self-taught Frontend developer with over 2 years of professional
+        experience in diverse industries{' '}
         <span>designing and developing interfaces</span>,{' '}
         <span>testing features</span>, and <span>pushing code</span>. Proficient
         in using a wide variety of tools to provide value and enhance user
